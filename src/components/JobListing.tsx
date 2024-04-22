9a0e1459a8f1a2d17b0e291621abc8917f901ea9
abdelrahmanaman <abdelrahman.bourka1@gmail.com>
2024-04-22 20:59:04 +0200

    adding styles for the joblisting component

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,6 +1,7 @@
 import { Job } from "@prisma/client";
 import Image from "next/image";
-import { BriefcaseBusiness } from 'lucide-react';
+import { Banknote, BriefcaseBusiness, Clock, Globe, MapPin } from 'lucide-react';
+import { formatCurrency, formatDate } from "@/lib/utils";
 interface JobListingProps {
   job: Job;
 }
@@ -18,7 +19,7 @@ const JobListing = ({
   },
 }: JobListingProps) => {
   return (
-    <article className=" flex cursor-pointer rounded-lg border-2 p-5 hover:bg-muted/60">
+    <article className="relative flex gap-4 items-center cursor-pointer rounded-lg border-2 p-5 hover:bg-muted/60">
       <Image
         src={companyLogoUrl || "/company-logo.svg"}
         alt={`${companyName} Logo`}
@@ -26,20 +27,37 @@ const JobListing = ({
         height={100}
         className="self-center rounded-lg"
       />
-
       <div className="flex-grow space-y-4 text-muted-foreground">
         <div>
           <h2 className="text-xl font-medium text-black">{title}</h2>
           <p>{companyName}</p>
         </div>
         <div className="">
-          <p className="flex items-center gap-2">
+          <p className="flex items-center gap-2 sm:hidden">
             <BriefcaseBusiness className="size-4 shrink-0" />
             {type}
-            
+          </p>
+          <p className="flex items-center gap-2">
+            <MapPin className="size-4 shrink-0" />
+            {locationType}
+          </p>
+          <p className="flex items-center gap-2">
+            <Globe className="size-4 shrink-0" />
+            {location || "Worldwide"}
+          </p>
+          <p className="flex items-center gap-2">
+            <Banknote className="size-4 shrink-0" />
+            {formatCurrency(salary)}
           </p>
         </div>
       </div>
+      <span className="absolute hidden md:flex top-6 font-medium right-8 rounded bg-muted text-sm p-2 text-muted-foreground">
+        {type}
+      </span>
+      <span className="absolute gap-1.5 bottom-2 sm:right-4 right-4  md:right-8 rounded-lg text-muted-foreground/75 flex items-center md:text-sm text-xs font-medium">
+        <Clock className="size-4" />
+        {formatDate(createdAt)}
+      </span>
     </article>
   );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+import { formatDistanceToNowStrict } from "date-fns";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+//* Currency Changer function
+export function formatCurrency(amount: number): string{
+  return new Intl.NumberFormat("fr-FR", {
+    style: "currency",
+    currency: "EUR"
+  }).format(amount);
+}
+
+//* Date formatter into text
+export function formatDate(from: Date): string {
+  return formatDistanceToNowStrict(from, { addSuffix: true });
 }
